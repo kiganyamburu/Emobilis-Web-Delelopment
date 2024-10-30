8c29e24e64959a5030efecba5885d6a11ddae97d
kiganyamburu <mburukiganya@gmail.com>
2024-10-30 13:15:27 +0300

    Your commit message here

--- Python-Basics/recap/test.py
+++ Python-Basics/recap/test.py
@@ -1,16 +1,72 @@
-def fibonacci(n):
-    if n < 0:
-        return "Please enter a non-negative number"
-    elif n <= 1:
-        return n
+# def fibonacci(n):
+#     if n < 0:
+#         return "Please enter a non-negative number"
+#     elif n <= 1:
+#         return n
+#
+#     a, b = 0, 1
+#     for _ in range(2, n + 1):
+#         a, b = b, a + b
+#     return b
+#
+# print(fibonacci(0))
+# print(fibonacci(1))
+# print(fibonacci(2))
+# print(fibonacci(3))
+# print(fibonacci(20))
 
-    a, b = 0, 1
-    for _ in range(2, n + 1):
-        a, b = b, a + b
-    return b
+# classes in python
 
-print(fibonacci(0))
-print(fibonacci(1))
-print(fibonacci(2))
-print(fibonacci(3))
-print(fibonacci(20))
+class animals:
+    def __init__(self, name, number_legs):
+        self.name = name
+        self.number_legs = number_legs
+
+    def move(self):
+        print("i can walk")
+
+class Dog(animals):
+    def sound(self):
+        print("I bark")
+
+my_animal = Dog("osama", 4)
+my_animal.sound()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
