92f4a759a9808541a79f6c763e1c0dcee250a559
kiganyamburu <mburukiganya@gmail.com>
2024-11-04 19:50:57 +0300

    pressing on with django

--- Django/school/school/settings.py
+++ Django/school/school/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'techers',
+    'teachers',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
