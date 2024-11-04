5aa76288a96f9663155f9d677162cbd6352dba32
kiganyamburu <mburukiganya@gmail.com>
2024-11-04 23:07:03 +0300

    car project

---
+++ Django/cars/bmw/views.py
@@ -0,0 +1,11 @@
+from operator import index
+
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
