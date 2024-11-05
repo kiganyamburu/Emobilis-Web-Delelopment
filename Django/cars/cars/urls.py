887da14b6a29ead9f7ecc5bd905dca37ec993f38
kiganyamburu <mburukiganya@gmail.com>
2024-11-05 12:29:07 +0300

    using jinja

--- Django/cars/cars/urls.py
+++ Django/cars/cars/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
 from bmw import views
 
 
--- Django/mid/mid/urls.py
+++ Django/mid/mid/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for school project.
+URL configuration for mid project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,19 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path
-# from teachers import views
-
-
 from django.contrib import admin
 from django.urls import path
-from teachers import views
+from morning import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('shop/', views.shop, name='shop'),
+
     path('about/', views.about, name='about'),
+
 
 ]
