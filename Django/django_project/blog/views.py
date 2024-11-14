61c27f655c0deb94f2884181fbcb42feabfd390d
kiganyamburu <mburukiganya@gmail.com>
2024-11-14 10:20:35 +0300

    posts

--- Django/django_project/blog/views.py
+++ Django/django_project/blog/views.py
@@ -3,9 +3,31 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'coreyMs',
+        'title': 'Blog Post',
+        'content': 'First post content' ,
+        'date_posted': 'August 27, 2018',
+    },
+
+    {
+        'author': 'jane doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content' ,
+        'date_posted': 'August 28, 2018',
+    }
+
+]
+
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>About page</h1>')
+    return render(request, 'blog/about.html')
