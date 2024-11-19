525e3181833bc81901dca9e7a2348af0519a91f2
kiganyamburu <mburukiganya@gmail.com>
2024-11-19 08:30:38 +0300

    post_confirm_delete

--- Django/django_project/blog/views.py
+++ Django/django_project/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import  Post
 
 # Create your views here.
@@ -45,6 +45,17 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
         if self.request.user == post.author:
             return True
         return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 
 
