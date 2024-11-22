fcd82f2ad91f60b987d616decedcc9d5a0bd2e55
kiganyamburu <mburukiganya@gmail.com>
2024-11-22 14:06:18 +0300

    post

--- Django/lecture3/lecture3/urls.py
+++ Django/lecture3/lecture3/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("hello/", include("hello.urls")),
